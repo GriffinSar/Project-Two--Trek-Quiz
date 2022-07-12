7719af1e6834f2a526fb701b4184b792fc95613b
GriffinSar <caladoniasaz@gmail.com>
2022-07-12 12:52:51 +0000

    Add function that sets the answer that the user selected to green if they selected correct or red if they selected wrong answer

--- assets/js/game.js
+++ assets/js/game.js
@@ -161,6 +161,15 @@ startGame = () => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"];
 
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    
+      selectedChoice.parentElement.classList.add(classToApply);
+      setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+      }, 1000);
+
     getNewQuestion();
     });
   });
