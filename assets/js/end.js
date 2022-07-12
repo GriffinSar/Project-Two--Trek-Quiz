8deab14782a120762b968d196b19df8af425c25b
GriffinSar <caladoniasaz@gmail.com>
2022-07-12 16:28:45 +0000

    Add style to center score on the end page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,5 +1,4 @@
 const finalScore = document.getElementById('final-score');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-console.log(mostRecentScore);
 
 finalScore.innerText = mostRecentScore;
