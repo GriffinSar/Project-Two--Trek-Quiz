30596fa49882d98f0c995865a44c1d89a61b890d
GriffinSar <caladoniasaz@gmail.com>
2022-07-13 15:29:03 +0000

    Create function so user can generate a random star trek name on the end page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,4 +1,22 @@
 const finalScore = document.getElementById('final-score');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const fName = document.querySelector(".fname");
+const lName = document.querySelector(".lname");
+const nameBTN = document.querySelector(".name-button");
 
 finalScore.innerText = mostRecentScore;
+
+const firstName = [
+    "Sarah", "Oisin", "Saoirse", "Eanna", "Frodo"]
+
+const lastName = [
+    "Walsh", "Griffin", "Leahy", "Phelan", "Smith"
+]
+
+nameBTN.addEventListener('click', function() {
+    fName.innerHTML =
+    firstName[Math.floor(Math.random() * firstName.length)];
+
+    lName.innerHTML = 
+    lastName[Math.floor(Math.random() * lastName.length)];
+});
